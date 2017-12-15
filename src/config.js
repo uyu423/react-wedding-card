d26ebe5e006f2e9358f98860aa7f7b84c86129e2
Yu Yongwoo <uyu423@gmail.com>
2017-12-16 07:06:22 +0900

    image, config.js, html update

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 const gallery = [ // galley image list
   require('./Image/gallery/1.png'),
-  require('./Image/gallery/2.jpg'),
-  require('./Image/gallery/3.jpg'),
-  require('./Image/gallery/4.jpg'),
-  require('./Image/gallery/5.jpg'),
+  require('./Image/gallery/2.png'),
+  require('./Image/gallery/3.png'),
+  require('./Image/gallery/4.png'),
+  require('./Image/gallery/5.png'),
   require('./Image/gallery/6.jpg'),
+  require('./Image/gallery/7.jpg'),
+  require('./Image/gallery/8.jpg'),
 ];
 
 export default {
@@ -25,33 +27,33 @@ export default {
   title: 'Wedding Invitation', // recomanded English
   wedding: {
     place: {
-      name: '예쁜 예식장 3F 아름다운홀',
-      address: '서울 영등포구 여의도동 60 63빌딩',
-      latitude: 37.51990006878117,
-      longitude: 126.94020220536675,
+      name: '샤이닝스톤컨벤션 B1 모닝컨벤션홀',
+      address: '서울특별시 영등포구 은행로 30 중소기업중앙회',
+      latitude: 37.5282002153,
+      longitude: 126.9226583218,
     },
     at: '2018-01-27 12:00',
   },
   bridal: {
-    name: '신사동',
-    image: require('./Image/bridal.jpeg'),
-    phone: '+821011111111',
-    facebook: false, // if false, not showing facebook icon
-    father: '서울시',
-    mother: '강남구',
+    name: '김광민',
+    image: require('./Image/bridal.png'),
+    phone: '01026871577',
+    facebook: 'https://www.facebook.com/profile.php?id=100009450246267', // if false, not showing facebook icon
+    father: '김명철',
+    mother: '장명화',
     position: '장남',
   },
   groom: {
-    name: '역곡동',
-    image: require('./Image/groom.jpeg'),
-    phone: '+821022222222',
+    name: '강진주',
+    image: require('./Image/groom.png'),
+    phone: '01051215262',
     facebook: 'https://www.facebook.com/saznna',
-    father: '부천시',
-    mother: '원미구',
-    position: '차녀',
+    father: '강석일',
+    mother: '이상미',
+    position: '장녀',
   },
   image: {
-    header: require('./Image/header.jpg'),
+    header: require('./Image/gallery/2.png'),
     gallery: gallery.map(item => ({
       original: item,
       thumbnail: item,
